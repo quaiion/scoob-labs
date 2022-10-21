8737a332a874c222a990fe6c369ae8a140e23b12
Korney Ivanishin <korney059@gmail.com>
2022-10-22 01:22:33 +0300

    move status declaration in process.c

--- process.c
+++ process.c
@@ -4,12 +4,12 @@
 #include <sys/wait.h>
 
 int main() {
+    int wstat;
     pid_t ch = fork();
     if (ch == 0) {
         sleep(15);
         printf("child process from child: %d\ninitial process from child: %d\n", getpid(), getppid());
     } else {
-        int wstat;
         wait(&wstat);
         printf("initial process from initial: %d\ninitial's parent process: %d\nchild process from initial: %d\n", getpid(), getppid(), ch);
     }
